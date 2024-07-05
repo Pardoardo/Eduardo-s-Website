844cc018ecfce353f7c3155ba1843ee8450a1522
Eduardo Bezerra <eduardo.escolar447@gmail.com>
2024-07-05 15:14:54 -0300

    Adicionando mais algumas rotas

--- server/router.js
+++ server/router.js
@@ -1,7 +1,13 @@
-//Esse script serve para redirecionar o usuário para rota possível do website
+//Esse script serve para redirecionar o usuário para todas as rotas possíveis dentro do website
 function Router(app) {
     app.get('/', (request, response) => {
-        response.send('Hello Worl')
+        response.send('Hello World')
+    })
+    app.get('/sobre', (request, response) => {
+        response.send('sobre mim')
+    })
+    app.get('/posts', (request, response) => {
+        response.send('Posts')
     })
 }
 
