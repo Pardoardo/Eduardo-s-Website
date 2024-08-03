2e75c75d8dc5f586651cdcc9e35112af66ea8657
Eduardo Bezerra <eduardo.escolar447@gmail.com>
2024-08-03 19:09:49 -0300

    Tornando a navegação mais eficiente

--- server/router.js
+++ server/router.js
@@ -1,17 +1,43 @@
 //Esse script serve para redirecionar o usuário para todas as rotas possíveis dentro do website
 
 function Router(app) {
+    app.set('trust proxy', false)
     app.get('/', (request, response) => {
-        response.sendFile(__dirname + '/pages/index.html');
-    })
-    app.get('/sobre', (request, response) => {
-        response.send('sobre mim')
-    })
-    app.get('/posts', (request, response) => {
-        response.send('Posts')
+        switch (request.url) {
+            case '/':
+                //homepage
+                response.status(200)
+                response.sendFile(__dirname + '/pages/index.html')
+            break
+
+            case '/sobre': 
+                response.status(200)
+                response.send('sobre mim')
+            break
+
+            case '/posts': 
+                response.status(200)
+                response.send('posts')
+            break
+
+            case '/projetos': 
+                response.status(200)
+                response.send('projetos')
+            break
+
+            case '/posts': 
+                response.status(200)
+                response.send('posts')
+            break
+        
+            default:
+                //página 404
+                response.status(404)
+                response.send('404')
+            break
+        }
     })
 }
-
 
 module.exports = {
     Router
